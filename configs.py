63311026626f3557af8cb3331c6645dac926d466
Duc Nguyen <hustducnv@gmail.com>
2021-06-15 10:59:24 +0700

    final

--- configs.py
+++ configs.py
@@ -12,7 +12,7 @@ class Config:
 class UniEnViConfig:
     PROBLEM = 'uni_en_vi'
     MODEL = 'transformer'
-    CHECKPOINT_PATH = os.path.join(Config.CHECKPOINTS_DIR, 'unienvi', 'model.ckpt-800000')
+    CHECKPOINT_PATH = os.path.join(Config.CHECKPOINTS_DIR, 'unienvi', 'model.ckpt-1000000')
     VOCAB_DIR = Config.VOCABS_DIR
     HPARAMS = 'transformer_base_single_gpu'
 
@@ -20,7 +20,7 @@ class UniEnViConfig:
 class UniViEnConfig:
     PROBLEM = 'uni_vi_en'
     MODEL = 'transformer'
-    CHECKPOINT_PATH = os.path.join(Config.CHECKPOINTS_DIR, 'univien', 'model.ckpt-573000')
+    CHECKPOINT_PATH = os.path.join(Config.CHECKPOINTS_DIR, 'univien', 'model.ckpt-1000000')
     VOCAB_DIR = Config.VOCABS_DIR
     HPARAMS = 'transformer_base_single_gpu'
 
